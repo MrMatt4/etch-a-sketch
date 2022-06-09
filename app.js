d95c98f8b4c1cb73217fb47ae31676bd22d57e63
Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>
2022-06-09 19:15:42 +0300

    Add prompt and dynamic generation of grid
    Signed-off-by: Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-for (let i = 0; i < 16; i++) {
-    const createdDiv = document.createElement('div');
-    createdDiv.classList.add('grid-boxes');
-    const containerDiv = document.querySelector('.grid-container');
-    containerDiv.appendChild(createdDiv);
+
+function createGrid(noOfGrids) {
+    noOfGrids = parseInt(noOfGrids);
+    if (noOfGrids > 100 || noOfGrids < 1) {
+        return "Error! Number can't be higher 100 or smaller than 1."
+    }
+    else {
+        for (let x = 0; x < noOfGrids; x++) {
+            const rowDivContainer = document.createElement('div');
+            const containerDiv = document.querySelector('.grid-container');
+            containerDiv.appendChild(rowDivContainer);
+            for (let i = 0; i < noOfGrids; i++) {
+                const createdDivRow = document.createElement('div');
+                createdDivRow.classList.add('grid-boxes');
+                rowDivContainer.appendChild(createdDivRow);
+            }
+        }
+    }
 }
+
+input = window.prompt("Input an integer from 1 to 100.");
+
+if (input) {
+    createGrid(input);
+}
+
+createGrid(16);
+
 

7f6c3d442909032b27b4d363c6b34a74456c3c05
Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>
2022-06-09 18:03:17 +0300

    Initial commit
    Signed-off-by: Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>

---
+++ app.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 15; i++) {
+    const createdDiv = document.createElement('div');
+    const containerDiv = document.querySelector('.grid-container');
+    containerDiv.appendChild(createdDiv);
+}
+

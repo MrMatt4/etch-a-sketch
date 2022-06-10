091c8a5f338e33f9a51c4b062628289acf9b06dc
Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>
2022-06-10 18:46:28 +0300

    Add slider function to create grid box dynamically
    Signed-off-by: Matei Dinca <mateidinca@Mateis-MacBook-Pro.local>

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+let gridCreated = false;
+
 
 function createGrid(noOfGrids) {
     noOfGrids = parseInt(noOfGrids);
+
+    if (gridCreated) {
+       let allRowGrids = document.querySelectorAll('row-grid');
+       const containerDiv = document.querySelector('.grid-container');
+
+       allRowGrids.forEach( function(element) {
+           while (element.firstElementChild) {
+               element.removeChild(element.lastElementChild);
+           }
+       });
+       while (containerDiv.firstElementChild) {
+           containerDiv.removeChild(containerDiv.lastElementChild);
+       }
+
+       gridCreated = false;
+    }
     if (noOfGrids > 100 || noOfGrids < 1) {
         return "Error! Number can't be higher 100 or smaller than 1."
     }
     else {
         for (let x = 0; x < noOfGrids; x++) {
             const rowDivContainer = document.createElement('div');
+            rowDivContainer.classList.add("row-grid");
             const containerDiv = document.querySelector('.grid-container');
             rowDivContainer.setAttribute('style','display:flex;flex-grow:1');
             containerDiv.appendChild(rowDivContainer);
@@ -17,11 +36,13 @@ function createGrid(noOfGrids) {
             }
         }
     }
+    gridCreated = true;
 }
 
 let slider = document.getElementById("gridSlider");
 
 slider.oninput = function() {
+
     createGrid(this.value);
 }
 
